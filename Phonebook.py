3f539eaa3a6a7f5f7bb7117580c70d63c288746f
HRE <eini.hamid@yahoo.com>
2022-01-25 10:34:28 +0330

    phonebook (texttable)

--- Phonebook.py
+++ Phonebook.py
@@ -1,4 +1,5 @@
 import json
+import texttable
 
 
 class Storage:
@@ -24,13 +25,17 @@ class Storage:
     def load(self, name):
         f = open('phonebook.json', "r+")
         found = False
-
         data = json.loads(f.read())
         thisdict = data['contacts']
+        table = texttable.Texttable(max_width=0)
+        table.set_cols_align(['l', 'c', 'c', 'r'])
+        table.set_cols_valign(['t', 'm', 'm', 'b'])
         for contact in thisdict:
             if contact['first_name'] == name:
-                print(contact)
+                table.add_rows([['id', 'first name', 'last name', 'phone'],
+                                [contact['id'], contact['first_name'], contact['last_name'], contact['phone']]])
             found = True
+        print(table.draw())
 
         if found == False:
             print(f"There's no contact Record in Phonebook with name = {name}")
